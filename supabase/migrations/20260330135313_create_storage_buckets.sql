eeb687f17d7820143cd272f74c08261c774e9c03
vadym <kopachev.vadym@gmail.com>
2026-03-30 16:54:35 +0300

    feat: create storage bucket for avatars with access policies for users

---
+++ supabase/migrations/20260330135313_create_storage_buckets.sql
@@ -0,0 +1,25 @@
+insert into storage.buckets (id, name, public)
+values ('avatars', 'avatars', true);
+
+create
+policy "Avatar images are publicly accessible"
+on storage.objects for
+select
+    using (bucket_id = 'avatars');
+
+create
+policy "Users can upload own avatar"
+on storage.objects for insert
+with check (
+  bucket_id = 'avatars' and
+  auth.uid()::text = (storage.foldername(name))[1]
+);
+
+create
+policy "Users can update own avatar"
+on storage.objects for
+update
+    using (
+    bucket_id = 'avatars' and
+    auth.uid()::text = (storage.foldername(name))[1]
+    );
